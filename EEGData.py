750ea30f14bd294ebe502cbeea0f93f1ae2177c7
Mocho <miguel.munguia_@hotmail.com>
2018-05-01 23:14:53 -0500

    labels

--- EEGData.py
+++ EEGData.py
@@ -11,7 +11,9 @@ from Channel import *
 
 class EEGData:
 
+
     def __init__(self, freq, time, chM, f, add, labels):
+        self.i = 0
         self.frequency = freq
         self.duration = time
         self.filterHz = f
@@ -21,6 +23,7 @@ class EEGData:
         self.channels = []
         self.fillChannels(labels)
         self. windows = []
+
 
     #adds a WindowEEG object to the list
     def addWindow(self, w):
@@ -42,15 +45,15 @@ class EEGData:
             new = self.concatenateWindows()
         return new
 
-    def copyChannel(self, ch, i, label):
-        channel = Channel(label[i], ch)
-        i += 1
+    def copyChannel(self, ch, label):
+        channel = Channel(label[self.i], ch)
+        self.i += 1
         self.channels.append(channel)
 
     def fillChannels(self, labels):
         #iterate channel matrix
-        i = 0
-        np.apply_along_axis(self.copyChannel, 1, self.channelMatrix, i, labels)
+        self.i = 0
+        np.apply_along_axis(self.copyChannel, 1, self.channelMatrix, labels)
 
     def getLabels(self):
         lbls = []
